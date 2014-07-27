bb4816d65b78d3df337b59578b32b543bc078912
Joseph Crowe <joseph.crowe@live.co.uk>
2014-07-27 02:08:03 +0100

    Still writing EmulateGCC.hs.

--- EmulateGCC.hs
+++ EmulateGCC.hs
@@ -1,12 +1,15 @@
 module EmulateGCC where
 
-import Prelude hiding (!!)
-import Data.List hiding (!!)
+import Prelude hiding ((!!), length)
+import Data.List hiding ((!!), length, splitAt)
 import Control.Applicative
+import Control.Monad
 
-import GCC(Size(..), EAdr(..), IAdr(..), Atom(..), Code(..), Arith(..))
+import GCC
 
-(!!) = genericIndex -- Needed since we are indexing on Word values.
+(!!)    = genericIndex
+length  = genericLength
+splitAt = genericSplitAt
 
 --------------------------------------------------------------------------------
 -- The state of an emulated GCC machine.
@@ -16,35 +19,42 @@ data GCC = GCC{
     gData   :: [Data],   -- Data stack.
     gFrame  :: [Frame],  -- Environment frame chain.
     gReturn :: [Return]} -- Control stack.
+  deriving Show
 
 -- An entry in a data stack, environment frame, or cons cell.
 data Data
   = DAtom Atom
   | DCons Data Data
   | DClos IAdr Frame
+  deriving Show
 
 -- An (immutable) environment frame, excluding the parent frame pointer.
-data Frame = Frame{
-    fDummy  :: Boolean,
-    fData   :: [Data]}
+data Frame
+  = Frame{ fData::[Data] }
+  | DummyFrame{ fSize::Size }
+  deriving Show
 
 -- An entry in the control stack.
 data Return
-  = Return{ rPC::IAdr, rFrame::[Frame] }
+  = ReturnClos{ rPC::IAdr, rFrame::[Frame] }
+  | ReturnJoin{ rPC::IAdr }
   | ReturnStop
+  deriving Show
 
 -- Reason for halting the machine.
-data Halt = HStop | HFault String
+data Halt
+  = HStop | HFault String
+  deriving Show
 
 --------------------------------------------------------------------------------
 -- Monadic computation within a GCC, potentially halting or producing a value.
-newtype RunGCC a = RunGCC (GCC -> (Either Halt a, GCC))
+newtype RunGCC a = RunGCC{ unRunGCC :: GCC -> (Either Halt a, GCC) }
 
 instance Monad RunGCC where
     RunGCC f >>= g
       = RunGCC $ \gcc -> case f gcc of
-            (Right x, gcc') -> let RunGCC h = g x in h gcc'
-            halted          -> halted
+            (Right x, gcc')   -> let RunGCC h = g x in h gcc'
+            (Left halt, gcc') -> (Left halt, gcc')
     return x
       = RunGCC $ \gcc -> (Right x, gcc)
 
@@ -57,73 +67,152 @@ instance Applicative RunGCC where
 
 -- The following are as Control.Monad.State.{get,gets,put,modify}:
 getGCC :: RunGCC GCC
-getGCC = RunGCC $ \gcc -> (Left gcc, gcc)
+getGCC = RunGCC $ \gcc -> (Right gcc, gcc)
 
 getsGCC :: (GCC -> a) -> RunGCC a
-getsGCC f = RunGCC $ \gcc -> (Left $ f gcc, gcc)
+getsGCC f = RunGCC $ \gcc -> (Right $ f gcc, gcc)
 
 putGCC :: GCC -> RunGCC ()
 putGCC gcc = RunGCC $ const (Right (), gcc)
 
 modifyGCC :: (GCC -> GCC) -> RunGCC ()
-modifyGCC f = RunGCC $ \gcc -> (Left (), f gcc)
+modifyGCC f = RunGCC $ \gcc -> (Right (), f gcc)
+
+-- Halt the computation normally.
+stop :: RunGCC a
+stop = RunGCC $ \gcc -> (Left $ HStop, gcc)
 
 -- Halt the computation erroneously.
 fault :: String -> RunGCC a
-fault str = RunGCC $ \gcc -> (Left $ Fault str, gcc)
+fault str = RunGCC $ \gcc -> (Left $ HFault str, gcc)
 
 --------------------------------------------------------------------------------
 -- Run the machine until it halts, returning the final machine state and the
 -- reason for halting.
 runGCC :: GCC -> (Halt, GCC)
-runGCC gcc = case stepGCC gcc of
-    (Just halt, gcc') -> (halt, gcc')
-    (Nothing,   gcc') -> runGCC gcc'
+runGCC gcc = case unRunGCC stepGCC gcc of
+    (Right _, gcc')   -> runGCC gcc'
+    (Left halt, gcc') -> (halt, gcc')
 
--- Execute a single instruction, returning the new machine state, and, if the
--- machine halted during this instruction, the reason for halting.
-stepGCC :: GCC -> (Maybe Halt, GCC)
-stepGCC gcc@GCC{ gCode=code, gPC=IAdr pc }
-  | pc >= length code = (Just $ HFault "PC out of bounds", gcc)
-  | otherwise         = stepGCC' (code!!pc) gcc{ gPC=IAdr(pc+1) }
+-- Execute a single instruction.
+stepGCC :: RunGCC ()
+stepGCC = do
+    gcc@GCC{ gCode=code, gPC=IAdr pc } <- getGCC
+    unless (pc < length code) (fault "PC out of bounds")
+    putGCC gcc{ gPC=IAdr(pc+1) }
+    stepGCC' (code !! pc)
 
 --------------------------------------------------------------------------------
 -- Execute the given instruction, assuming the PC has already been incremented.
-stepGCC' :: Code -> GCC -> (Maybe Halt, GCC)
+stepGCC' :: Code -> RunGCC ()
 
-stepGCC' (LDC atom) gcc@GCC{ gData=ds }
-  = (Nothing, gcc{ gData=(DAtom atom):ds })
+stepGCC' (LDC atom) = do
+    push (DAtom atom)
 
-stepGCC' (LD (FAdr fAdr) (EAdr eAdr)) gcc@GCC{ gData=ds, gFrame=fs }
-  | fAdr >= length fs = (Just $ HFault "frame number out of bounds", gcc)
-  | dummy             = (Just $ HFault "frame mismatch", gcc)
-  | eAdr >= length es = (Just $ HFault "environment index out of bounds", gcc)
-  | otherwise         = (Nothing, gcc{ gData=(es !! eAdr):ds })
-  where Frame{ fData=es, fDummy=dummy } = fs !! fAdr
+stepGCC' (LD (FAdr fAdr) (EAdr eAdr)) = do
+    gcc@GCC{ gData=ds, gFrame=fs } <- getGCC
+    unless (fAdr < length fs) (fault "frame number out of bounds")
+    case fs !! fAdr of
+        Frame{ fData=es } -> do
+            unless (eAdr < length es) (fault "environment index out of bounds")
+            push (es !! eAdr)
+        _ -> fault "frame mismatch"
 
-stepGCC' (CArith op) gcc = case gData gcc of
-    DAtom x : DAtom y : ds -> (Nothing, gcc{ gData=(arith op x y):ds })
-    _ : _ : ds             -> (Just $ HFault "type mismatch", gcc{ gData=ds })
-    _                      -> (Just $ HFault "stack underflow", gcc)
+stepGCC' (CArith op) = do
+    [x, y] <- popN 2
+    case (x,y) of
+        (DAtom x', DAtom y') -> push (DAtom $ arith op x' y')
+        _                    -> fault "type mismatch"
 
-stepGCC' ATOM gcc = case gData gcc of
-    DAtom _ : ds -> (Nothing, gcc{ gData=DAtom(Atom 1):ds })
-    _ : ds       -> (Nothing, gcc{ gData=DAtom(Atom 0):ds })
-    _            -> (Just $ HFault "stack underflow", gcc)
+stepGCC' ATOM = do
+    d <- pop
+    let bool = case d of { DAtom _ -> 1; _ -> 0 }
+    push $ DAtom (Atom bool)
 
-stepGCC' CONS gcc = case gData gcc of
-    x : y : ds -> (Nothing, gcc{ gData=(DCons x y):ds })
-    _          -> (Just $ HFault "stack underflow", gcc)
+stepGCC' CONS = do
+    [x, y] <- popN 2
+    push $ DCons x y
 
-stepGCC' CAR gcc = case gData gcc of
-    DCons x _ : ds  -> (Nothing, gcc{ gData=x:ds })
-    _ : ds          -> (Just $ HFault "type mismatch", gcc{ gData=ds })
-    _               -> (Just $ HFault "stack underflow", gcc)
+stepGCC' CAR = do
+    d <- pop
+    case d of
+        DCons x _ -> push x
+        _         -> fault "type mismatch"
 
-stepGCC' CDR gcc = case gData gcc of
-    DCons _ y : ds  -> (Nothing, gcc{ gData=y:ds })
-    _ : ds          -> (Just $ HFault "type mismatch", gcc{ gData=ds })
-    _               -> (Just $ HFault "stack underflow", gcc)
+stepGCC' (SEL (IAdr trueAddr) (IAdr falseAddr)) = do
+    d <- pop
+    case d of
+        DAtom bool -> modifyGCC $ \gcc@GCC{ gReturn=rs, gPC=pc } ->
+            gcc{ gPC = if bool /= 0 then trueAddr else falseAddr,
+                 gReturn = ReturnJoin{ rPC=pc } }
+        _ -> fault "type mismatch"
+
+stepGCC' JOIN = do
+    r <- popReturn
+    case r of
+        ReturnJoin{ rPC=pc } -> modifyGCC $ \gcc -> gcc{ gPC=pc }
+        _                    -> fault "control mismatch"
+
+stepGCC' (LDF (IAdr iAdr)) = do
+    fs <- getsGCC gFrame
+    push $ DClos iAdr fs
+
+stepGCC' (AP (Size argSize)) = do
+    gcc@GCC{ gData=ds, gFrame=fs, gReturn=rs, gPC=pc } <- getGCC
+    d <- pop
+    case d of
+        DClos iAdr fs' -> do
+            args <- popN argSize
+            putGCC gcc{ gPC=iAdr,
+                        gFrame=Frame{ fData=args }:fs',
+                        gReturn=ReturnClos{ rPC=pc, rFrame=fs }:rs }
+        _ -> fault "type mismatch"
+
+stepGCC' RTN = do
+    r <- popReturn
+    case r of
+        ReturnClos{ rPC=pc, rFrame=fs } ->
+            modifyGCC $ \gcc -> gcc{ gPC=pc, gFrame=fs }
+        ReturnStop -> stop
+        _          -> fault "control mismatch"
+
+stepGCC' (DUM dumSize) = do
+    modifyGCC $ \gcc@GCC{ gFrame=fs } ->
+        gcc{ gFrame=DummyFrame{ fSize=dumSize }:fs }
+
+stepGCC' (RAP (Size argSize)) = do
+    d <- pop
+    gcc@GCC{ gFrame=fs }
+    case (d,fs) of
+        (DClos iAdr fs', :) ->
+            
+        _ -> fault "type mismatch"
+
+-- Remove a value from atop the data stack and return it.
+pop :: RunGCC Data
+pop = head <$> popN 1
+
+-- Remove n values from atop the data stack and return them in reverse order.
+popN :: Integral i => i -> RunGCC [Data]
+popN n = do
+    gcc@GCC{ gData=ds } <- getGCC
+    let (ds',ds'') = splitAt n ds
+    putGCC gcc{ gData=ds'' }
+    unless (length ds' == n) (fault "data stack underflow")
+    return (reverse ds')
+
+-- Insert a value at the top of the data stack.
+push :: Data -> RunGCC ()
+push d = do
+    modifyGCC $ \gcc@GCC{ gData=ds } -> gcc{ gData=d:ds }
+
+-- Remove the top entry of the control stack and return it.
+popReturn :: RunGCC Return
+popReturn = do
+    gcc@GCC{ gReturn=rs } <- getGCC
+    case rs of
+        r:rs' -> putGCC gcc{ gReturn=rs' } >> return r
+        _     -> fault "control stack underflow"
 
 --------------------------------------------------------------------------------
 -- Perform a binary arithmetic operation.
--- GCC.hs
+++ GCC.hs
@@ -1,23 +1,27 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module GCC where
 
-import Int(Int32)   -- Signed 32-bit integer.
-import Word(Word32) -- Unsigned 32-bit integer.
+import Data.Int(Int32)   -- Signed 32-bit integer.
+import Data.Word(Word32) -- Unsigned 32-bit integer.
 
-newtype Size = Size Word32
-newtype FAdr = FAdr Word32  -- Frame address within a frame chain.
-newtype EAdr = EAdr Word32  -- Data address within an environment frame.
-newtype IAdr = IAdr Word32  -- Instruction address.
-newtype Atom = Atom Int32   -- Atomic data value.
+newtype Size = Size Word32 deriving (Num, Show)
+newtype FAdr = FAdr Word32 deriving (Num, Show) -- Frame address within a chain.
+newtype EAdr = EAdr Word32 deriving (Num, Show) -- Data address within frame.
+newtype IAdr = IAdr Word32 deriving (Num, Show) -- Instruction address.
+newtype Atom = Atom Int32  deriving (Num, Show) -- Atomic data value.
 
 data Code
-  = LDC Word | LD FAdr EAdr
+  = LDC Atom | LD FAdr EAdr
   | CArith Arith
   | ATOM | CONS | CAR | CDR
   | SEL | JOIN
   | LDF IAdr | AP Size | RTN
   | DUM Size | RAP Size
   | STOP
+  deriving Show
 
 data Arith
   = ADD | SUB | MUL | DIV
   | CEQ | CGT | CGTE
+  deriving Show
